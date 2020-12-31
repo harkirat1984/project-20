38a2ba682442fe950ca277e9c916f0f3e5d2e293
harkirat1984 <71445577+harkirat1984@users.noreply.github.com>
2020-12-31 12:17:17 +0530

    Add files via upload
    hi 
    
    
    happy new year (2021)

---
+++ GCSO-master/GCSO-master/sketch.js
@@ -0,0 +1,40 @@
+var speed;
+var weight; 
+var car;
+var wall;
+function setup() {
+createCanvas(1600, 400);
+speed=random(55,90)
+weight=random(400,1500)
+car=createSprite(50, 200, 50,50);   
+car.velocityX = speed;
+car.shapeColor=color(255);
+wall=createSprite(1500,200, 60, height/2)
+wall.shapeColor=color("red");
+}
+function preload(){
+//nothing i dont have images 	
+};
+function draw() {
+background("brown");
+if(wall.x-car.x < (car.width+wall.width)/2)
+{
+car.velocityX=0;
+var deformation=0.5 * weight * speed* speed/22500;
+if(deformation>180)
+{
+car.shapeColor=color("blue");
+}
+
+if(deformation<180 && deformation>100)
+{
+car.shapeColor=color("grey");
+}
+
+if(deformation<100)
+{
+car.shapeColor=color("green");
+}
+}  
+drawSprites();
+}
